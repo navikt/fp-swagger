2219938df920a3c1c781147949073e627daed469
Michal J. Sladek <michal.sladek@nav.no>
2022-11-17 13:58:09 +0100

    Fikser en del lint problemer.

--- server/cache/token-cache.js
+++ server/cache/token-cache.js
@@ -1,7 +1,9 @@
 import NodeCache from 'node-cache';
 
+// eslint-disable-next-line no-underscore-dangle,import/no-mutable-exports
 let _tokenCache;
 
+// eslint-disable-next-line prefer-const
 _tokenCache = _tokenCache || new NodeCache();
 
 export default _tokenCache;
--- server/utils/oidcUtils.js
+++ server/utils/oidcUtils.js
@@ -1,6 +1,2 @@
-
-export const createOidcUnknownError = (err) =>
-  `Noe gikk galt med token exchange mot Azure.
-   Feilmelding fra openid-client: (${err}).
-   HTTP Status fra Azure: (${err.response?.statusCode} ${err.response?.statusMessage})
-   Body fra Azure: ${JSON.stringify(err.response?.body)}`;
+// eslint-disable-next-line import/prefer-default-export
+export const createOidcUnknownError = (err) => `Noe gikk galt med token exchange mot Azure. Feilmelding fra openid-client: (${err}).`;
